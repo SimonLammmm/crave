e8dbdde4df90c84e8b07481470d3d9c344010bfe
SimonLammmm <77494514+SimonLammmm@users.noreply.github.com>
2026-06-24 10:23:51 +0100

    Delete shiny-server/notices.R

--- shiny-server/notices.R
+++
@@ -1,9 +0,0 @@
-# Temporary notices
-# Bind this file into the container at /src/notices.R
-# Notices in this file will be shown as banners on the front page
-
-notices_info <- NULL
-
-notices_warning <- NULL
-
-notices_emergency <- NULL
